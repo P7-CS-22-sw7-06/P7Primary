6fc43f3f17f1d2a63c6f6df5f7dccb253acb7015
Magnus Mathiesen <magnus_mat@pm.me>
2023-02-20 14:16:48 +0100

    Fixed Install Script

--- Scripts/Install Criu & Docker.sh
+++ Scripts/Install Criu & Docker.sh
@@ -10,7 +10,7 @@ sudo apt update -y && sudo apt upgrade -y
 sudo add-apt-repository -y universe
 sudo add-apt-repository -y multiverse
 
-sudo apt install -y build-essential python-is-python3 python3-pip
+sudo apt install -y build-essential python3-pip
 
 sudo apt update -y && sudo apt upgrade -y
 
@@ -18,13 +18,11 @@ git clone https://github.com/checkpoint-restore/criu
 
 cd criu
 
-sudo apt install -y libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler \
-  protobuf-compiler python3-protobuf
+sudo apt install -y libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python3-protobuf
 
-pip install ipaddress
+pip3 install ipaddress
 
-sudo apt install -y pkg-config libbsd-dev iproute2 libnftables-dev libcap-dev \
-  libnet1-dev libnl-3-dev libnet-dev libaio-dev libgnutls28-dev python3-future
+sudo apt install -y pkg-config libbsd-dev iproute2 libcap-dev libnet1-dev libnl-3-dev libnet-dev libaio-dev libgnutls28-dev python3-future
 
 sudo apt install -y asciidoc xmlto
 
@@ -41,8 +39,7 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt-get install ca-certificates curl gnupg lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
@@ -52,8 +49,7 @@ sudo apt update -y && sudo apt upgrade -y
 
 DOCKER_VERSION=$(apt-cache madison docker-ce | awk '{ print $3 }' | grep '17')
 
-sudo apt-get install docker-ce=${DOCKER_VERSION} \
-  docker-ce-cli=${DOCKER_VERSION} containerd.io docker-compose-plugin
+sudo apt-get install docker-ce=${DOCKER_VERSION} docker-ce-cli=${DOCKER_VERSION} containerd.io docker-compose-plugin
 
 sudo apt-mark hold docker-ce
 sudo apt-mark hold docker-ce-cli
